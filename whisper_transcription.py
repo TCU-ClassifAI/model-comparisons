94b230c203f00c1e02883bb9d7d3a7468ff0c1c2
John Mejia <29128358+jhmejia@users.noreply.github.com>
2023-10-22 10:52:10 -0500

    add json formatting

--- whisper_transcription.py
+++ whisper_transcription.py
@@ -1,4 +1,5 @@
 import torch
+import json
 
 #cuda
 if not torch.cuda.is_available():
@@ -9,6 +10,12 @@ import whisper
 model = whisper.load_model("large") # Runs largev2 
 result = model.transcribe("Linux-9-28-2023.m4a")
 
+
+# Save the result as a JSON file
+with open("result.json", "w") as f:  # Use "result.json" instead of "result.txt"
+    # Serialize the result as JSON and write it to the file
+    json.dump(result, f, indent=4)
+
 # Save result to file
 with open("result.txt", "w") as f:
-    f.write(result)
+    f.write(result["text"])
